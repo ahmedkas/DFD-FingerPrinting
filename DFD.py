0efc20c75b6302084a75feec5244ec3e746c78f3
Ahmed Abusnaina <ceng.ahmed.kas@gmail.com>
2021-01-15 17:15:22 -0500

    Update DFD.py

--- DFD.py
+++ DFD.py
@@ -5,15 +5,16 @@ import random
 # X_test: the data to be perturbed
 X_test = load()
 
-# pert: the perturbation rate
+# pert: the perturbation rate (positive number)
 pert = 0.50
-# variation_ratio: the variation of the perturbation at each burst, this will only effect the white-box attack accuracy.
+
+# variation_ratio: the variation of the perturbation at each burst, this will only effect the white-box attack accuracy. (0 < variation_ratio < 1.0)
 variation_ratio = 0.5
 
-# ServerSide: If True, server side injection will also be used
+# ServerSide: If True, server side injection will also be used (True/False)
 ServerSide = True
 
-# ClientSide: If True, client side injection will also be used
+# ClientSide: If True, client side injection will also be used (True/False)
 ClientSide = True
 
 # X_testN: the new perturbed representation
@@ -50,6 +51,6 @@ for i in range(len(X_test)):
                             countAdded += 1
                     Cout = 0
         last = X_test[i][j]
-    X_testN[i] = X_testN[i][:5000]
+    X_testN[i] = X_testN[i][:5000] # The representation cut traces above 5,000 packets
 
 # X_testN contains the perturbed data.
