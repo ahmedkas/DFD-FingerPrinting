464b42115474c5b59b03c3aee0e2a18cad1dd28b
Ahmed Abusnaina <ceng.ahmed.kas@gmail.com>
2021-01-15 19:05:33 -0500

    Update DFD.py

--- DFD.py
+++ DFD.py
@@ -3,24 +3,24 @@ import numpy as np
 import random
 
 # The data should be loaded from https://github.com/deep-fingerprinting/df, and used here.
-# X_test: the data to be perturbed, should be a matrix of Nx5000
-# N refer to the number of traces, where each trace are of values {-1,0,1}
+# X_test: The original data, a matrix of shape Nx5000
+# N refer to the number of traces, where each trace contains values within {-1,0,1}
 f = open("./sampleInput.pickle","rb")
 X_test = pickle.load(f)# Data
 
-# pert: the perturbation rate (positive number)
+# pert: the perturbation rate to be applied
 pert = 0.50
 
-# variation_ratio: the variation of the perturbation at each burst, this will only effect the white-box attack accuracy. (0 < variation_ratio < 1.0)
+# variation_ratio: the variation parameter, affect the perturbation rate at the begining of each burst, this will only effect the white-box attack accuracy. (0 < variation_ratio < 1.0)
 variation_ratio = 0.5
 
-# ServerSide: If True, server side injection will also be used (True/False)
+# ServerSide: If True, server-side injection will also be used (True/False)
 ServerSide = True
 
-# ClientSide: If True, client side injection will also be used (True/False)
+# ClientSide: If True, client-side injection will also be used (True/False)
 ClientSide = True
 
-# X_testN: the new perturbed representation
+# X_testN: the perturbed representation
 X_testN = []
 for i in range(len(X_test)):
 
@@ -51,9 +51,9 @@ for i in range(len(X_test)):
                             X_testN[i].append(1)
                     Cout = 0
         last = X_test[i][j]
-    X_testN[i] = X_testN[i][:5000] # The representation cut traces above 5,000 packets
+    X_testN[i] = X_testN[i][:5000] # Limit to the first 5,000 packets
 
-# X_testN contains the perturbed data.
+# X_testN: the perturbed data.
 X_testN = np.asarray(X_testN)
 f = open("./sampleOutput.pickle","wb")
 pickle.dump(X_testN,f)
